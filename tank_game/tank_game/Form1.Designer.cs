95ef6b4b1edf644b97907583c5c7ec4a43dda42d
Perry <49940223+parry1233@users.noreply.github.com>
2020-10-02 21:34:50 +0800

    Add files via upload

---
+++ tank_game/tank_game/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace tank_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.enemy1 = new System.Windows.Forms.PictureBox();
+            this.enemy2 = new System.Windows.Forms.PictureBox();
+            this.enemy3 = new System.Windows.Forms.PictureBox();
+            this.bullet = new System.Windows.Forms.PictureBox();
+            this.fighter = new System.Windows.Forms.PictureBox();
+            this.scoretext = new System.Windows.Forms.Label();
+            this.playtimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fighter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // enemy1
+            // 
+            this.enemy1.Image = ((System.Drawing.Image)(resources.GetObject("enemy1.Image")));
+            this.enemy1.Location = new System.Drawing.Point(12, 11);
+            this.enemy1.Name = "enemy1";
+            this.enemy1.Size = new System.Drawing.Size(102, 90);
+            this.enemy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy1.TabIndex = 0;
+            this.enemy1.TabStop = false;
+            // 
+            // enemy2
+            // 
+            this.enemy2.Image = ((System.Drawing.Image)(resources.GetObject("enemy2.Image")));
+            this.enemy2.Location = new System.Drawing.Point(250, 12);
+            this.enemy2.Name = "enemy2";
+            this.enemy2.Size = new System.Drawing.Size(102, 90);
+            this.enemy2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy2.TabIndex = 1;
+            this.enemy2.TabStop = false;
+            // 
+            // enemy3
+            // 
+            this.enemy3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.enemy3.Image = ((System.Drawing.Image)(resources.GetObject("enemy3.Image")));
+            this.enemy3.Location = new System.Drawing.Point(496, 12);
+            this.enemy3.Name = "enemy3";
+            this.enemy3.Size = new System.Drawing.Size(102, 90);
+            this.enemy3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy3.TabIndex = 2;
+            this.enemy3.TabStop = false;
+            // 
+            // bullet
+            // 
+            this.bullet.Image = ((System.Drawing.Image)(resources.GetObject("bullet.Image")));
+            this.bullet.Location = new System.Drawing.Point(294, 517);
+            this.bullet.Name = "bullet";
+            this.bullet.Size = new System.Drawing.Size(7, 27);
+            this.bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bullet.TabIndex = 3;
+            this.bullet.TabStop = false;
+            // 
+            // fighter
+            // 
+            this.fighter.Image = ((System.Drawing.Image)(resources.GetObject("fighter.Image")));
+            this.fighter.Location = new System.Drawing.Point(241, 550);
+            this.fighter.Name = "fighter";
+            this.fighter.Size = new System.Drawing.Size(111, 97);
+            this.fighter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fighter.TabIndex = 4;
+            this.fighter.TabStop = false;
+            // 
+            // scoretext
+            // 
+            this.scoretext.AutoSize = true;
+            this.scoretext.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.scoretext.Font = new System.Drawing.Font("微軟正黑體", 40.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.scoretext.ForeColor = System.Drawing.Color.Red;
+            this.scoretext.Location = new System.Drawing.Point(279, 311);
+            this.scoretext.Name = "scoretext";
+            this.scoretext.Size = new System.Drawing.Size(213, 86);
+            this.scoretext.TabIndex = 5;
+            this.scoretext.Text = "score";
+            this.scoretext.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // playtimer
+            // 
+            this.playtimer.Enabled = true;
+            this.playtimer.Interval = 10;
+            this.playtimer.Tick += new System.EventHandler(this.playtimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(622, 659);
+            this.Controls.Add(this.fighter);
+            this.Controls.Add(this.bullet);
+            this.Controls.Add(this.enemy3);
+            this.Controls.Add(this.enemy2);
+            this.Controls.Add(this.enemy1);
+            this.Controls.Add(this.scoretext);
+            this.Name = "Form1";
+            this.Text = "BattlePlane";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.onKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.onKeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fighter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox enemy1;
+        private System.Windows.Forms.PictureBox enemy2;
+        private System.Windows.Forms.PictureBox enemy3;
+        private System.Windows.Forms.PictureBox bullet;
+        private System.Windows.Forms.PictureBox fighter;
+        private System.Windows.Forms.Label scoretext;
+        private System.Windows.Forms.Timer playtimer;
+    }
+}
+
---
+++ tank_game/tank_game/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tank_game
+{
+    public partial class Form1 : Form
+    {
+        int moveLeft = 0;
+        int enemyMove = 5;
+        Random random = new Random();
+        int bulletSpeed = 10;
+        bool shooting = false;
+        int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            enemy1.Top = -500;
+            enemy2.Top = -900;
+            enemy3.Top = -1300;
+            bullet.Top = -100;
+            bullet.Left = -100;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                if (fighter.Location.X <0)
+                {
+                    moveLeft = 0;
+                }
+                else
+                {
+                    moveLeft = -10;
+                }
+            }
+
+            else if (e.KeyCode == Keys.Right)
+            {
+                if(fighter.Location.X>510)
+                {
+                    moveLeft = 0;
+                }
+                else
+                {
+                    moveLeft = 10;
+                }
+            }
+
+            else if(e.KeyCode==Keys.Space)
+            {
+                if (shooting == false)
+                {
+                    bulletSpeed = 10;
+                    bullet.Left = fighter.Left + 50;
+                    bullet.Top = fighter.Top;
+                    shooting = true;
+                }
+            }
+
+        }
+
+        private void onKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = 0;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                moveLeft = 0;
+            }
+        }
+
+        private void playtimer_Tick(object sender, EventArgs e)
+        {
+            fighter.Left += moveLeft;
+            bullet.Top -= bulletSpeed;
+            enemy1.Top += enemyMove;
+            enemy2.Top += enemyMove;
+            enemy3.Top += enemyMove;
+            scoretext.Text = "" + score;
+
+            if (enemy1.Top == 660 || enemy2.Top == 660 || enemy3.Top == 660)
+            {
+                GameOver();
+            }
+            if (shooting && bullet.Top < 0)
+            {
+                shooting = false;
+                bulletSpeed = 0;
+                bullet.Top = -100;
+                bullet.Left = -100;
+            }
+            EnemyHit();
+        }
+
+        private void EnemyHit()
+        {
+            if (bullet.Bounds.IntersectsWith(enemy1.Bounds))
+            {
+                score += 1;
+                enemy1.Top = -500;
+                int ranP = random.Next(1, 300);
+                enemy1.Left = ranP;
+                if (enemy1.Left < 0)
+                { enemy1.Left += 100; }
+                else if (enemy1.Left > 510)
+                { enemy1.Left -= 100; }
+                shooting = false;
+                bulletSpeed = 0;
+                bullet.Top = -100;
+                bullet.Left = -100;
+
+            }
+            else if (bullet.Bounds.IntersectsWith(enemy2.Bounds))
+            {
+                score += 1;
+                enemy2.Top = -900;
+                int ranP = random.Next(1, 400);
+                enemy2.Left = ranP;
+                if (enemy2.Left < 0)
+                { enemy2.Left += 100; }
+                else if (enemy2.Left > 510)
+                { enemy2.Left -= 100; }
+                shooting = false;
+                bulletSpeed = 0;
+                bullet.Top = -100;
+                bullet.Left = -100;
+
+            }
+            else if (bullet.Bounds.IntersectsWith(enemy3.Bounds))
+            {
+                score += 1;
+                enemy3.Top = -1300;
+                int ranP = random.Next(1, 500);
+                enemy3.Left = ranP;
+                if (enemy3.Left < 0)
+                { enemy3.Left += 100; }
+                else if (enemy3.Left > 510)
+                { enemy3.Left -= 100; }
+                shooting = false;
+                bulletSpeed = 0;
+                bullet.Top = -100;
+                bullet.Left = -100;
+            }
+        }
+
+        private void GameOver()
+        {
+            playtimer.Enabled = false;
+            MessageBox.Show("糟了!你被擊敗了!\n你這次擊墜了 " + score + "架敵機\n按空白鍵或確認來再玩一次","遊戲結算");
+            score = 0;
+            scoretext.Text = "0";
+            enemy1.Top = -500;
+            enemy2.Top = -900;
+            enemy3.Top = -1300;
+            bullet.Top = -100;
+            bullet.Left = -100;
+            playtimer.Enabled = true;
+        }
+    }
+}
